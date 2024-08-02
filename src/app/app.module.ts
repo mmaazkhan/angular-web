8ac42625bd260340fada67d2f704e5b1ed7b7576
mmaazkhna <maazkotha007@gmail.com>
2024-08-02 12:24:45 +0500

    files updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { AppDemoComponent } from './app-demo/app-demo.component';  // Ensure the path is correct
+import { AppDemoComponent } from './src/app/demo/demo.component';  // Ensure the path is correct
 
 @NgModule({
   declarations: [
